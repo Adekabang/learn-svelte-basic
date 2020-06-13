46600745880f0dd6b5bcfe9ab65b3b5003588f86
Mohammad Raska <iban.ninja@gmail.com>
2020-06-13 23:05:04 +0700

    initial svelte app and make emoji generator boilerplate

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import EmojiDisplay from './EmojiDisplay.svelte';
+</script>
+
+<div>
+	<h1>Randomizer Emoji</h1>
+	<EmojiDisplay />
+	<button>🔁 randomizer</button>
+</div>
+
+<style>
+	button {
+		background-color: #8bd3dd;
+		padding: 0.75em;
+		border-radius: 0.25em;
+		border: 2px solid #000000;
+		box-shadow: 0.4rem 0.4rem 0 #222;
+	}
+	button:hover{
+		box-shadow: 0.25rem 0.25rem 0 #222;
+		transition: all 0.4s ease 0s;
+		background-color: #8bd;
+	}
+	div{
+		margin: 2em;
+	}
+</style>
---
+++ src/EmojiDisplay.svelte
@@ -0,0 +1,15 @@
+<script>
+	let currentEmoji = '😊';
+	setTimeout(() => {
+		currentEmoji = '😢'
+	}, 1000);
+</script>
+
+<style>
+  div{
+    font-size: 3em;
+    margin-bottom: 0.5em;
+  }
+</style>
+
+<div>{currentEmoji}</div>
